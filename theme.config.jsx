66046367319285817d5a110701f44c2e50058b2f
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2023-08-19 00:59:25 -0700

    Nextra testing.

---
+++ theme.config.jsx
@@ -0,0 +1,21 @@
+import {useState} from "react";
+
+export default {
+    logo: <span>ShokoDocs</span>,
+    project: {
+        link: 'https://github.com/shuding/nextra'
+    },
+    chat: {
+        link: 'https://google.com'
+    },
+    sidebar: {
+        toggleButton: true,
+    },
+    gitTimestamp: 'Last Updated On',
+    navigation: {
+        prev: true,
+        next: true,
+
+    }
+
+}
